b9f65fb01b9b1da03b1d02af8e3d273d0fddab93
John C Barstow <jcb@jbowtie.geek.nz>
2020-12-13 11:35:15 +1300

    Rough out our initial test shape

--- tests/web.rs
+++ tests/web.rs
@@ -11,3 +11,23 @@ wasm_bindgen_test_configure!(run_in_browser);
 fn pass() {
     assert_eq!(1 + 1, 2);
 }
+/*
+// test the 3x3 blinker oscillator
+#[wasm_bindgen_test]
+pub fn test_blinker_1() {
+    // Let's create a grid in known state
+    let mut input_grid = input_state();
+
+    // expected state after one generation
+    let expected_grid = expected_state();
+
+    // advance the clock one generation
+    input_grid.tick();
+
+    // verify the state
+    assert_eq!(&input_grid.get_cells(), &expected_grid.get_cells());
+}
+*/
+// test the 4x8 from JP's example
+// test the 4x3 beehive still life
+// test a c/4 glider spaceship
