7b4730cd8a4ca1d23b3e82cf88bd0dc64a0aed6d
John C Barstow <jcb@jbowtie.geek.nz>
2020-12-13 17:30:27 +1300

    Count neighbors correctly with tests

--- src/lib.rs
+++ src/lib.rs
@@ -35,7 +35,7 @@ pub struct Grid {
 
 //functions in this section are Rust-only
 impl Grid {
-    // used by test
+    // used by test for comparison
     pub fn get_cells(&self) -> &[Cell] {
         &self.cells
     }
@@ -68,9 +68,24 @@ impl Grid {
         grid
     }
 
-    // TODO: real implementation
-    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
-        0
+    // pub for testing purposes
+    pub fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+
+                // avoid need to special case edges
+                // means we have wraparound but that's fun to look at
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_col = (column + delta_col) % self.width;
+                let idx = self.get_index(neighbor_row, neighbor_col);
+                count += self.cells[idx] as u8;
+            }
+        }
+        count
     }
 }
 
--- tests/web.rs
+++ tests/web.rs
@@ -33,8 +33,40 @@ pub fn test_live_grid_creation() {
     assert_eq!(input_grid.to_string(), expected);
 }
 
+#[wasm_bindgen_test]
+pub fn test_count_0_neighbors() {
+    let input_grid = Grid::create(3, 3, &[(1,1)]);
+    let count = input_grid.live_neighbor_count(1, 1);
+    assert_eq!(count, 0);
+}
+#[wasm_bindgen_test]
+pub fn test_count_1_neighbor() {
+    let input_grid = Grid::create(3, 3, &[(0,0)]);
+    let count = input_grid.live_neighbor_count(1, 1);
+    assert_eq!(count, 1);
+}
+#[wasm_bindgen_test]
+pub fn test_count_2_neighbors() {
+    let input_grid = Grid::create(3, 3, &[(0,1), (2,1)]);
+    let count = input_grid.live_neighbor_count(1, 1);
+    assert_eq!(count, 2);
+}
+#[wasm_bindgen_test]
+pub fn test_count_8_neighbors() {
+    let input_grid = Grid::create(3, 3, &[(0,0), (0,1),(0,2),(1,0),(1,2),(2,0),(2,1),(2,2)]);
+    let count = input_grid.live_neighbor_count(1, 1);
+    assert_eq!(count, 8);
+}
+#[wasm_bindgen_test]
+pub fn test_count_2_edge_neighbors() {
+    let input_grid = Grid::create(3, 3, &[(0,1),(1,1), (2,1)]);
+    let count = input_grid.live_neighbor_count(0, 1);
+    assert_eq!(count, 2);
+}
+
 
 // test the 3x3 blinker oscillator
+// vert-to-horiz
 #[wasm_bindgen_test]
 pub fn test_blinker_1() {
     // Let's create a grid in known state
@@ -42,6 +74,23 @@ pub fn test_blinker_1() {
 
     // expected state after one generation
     let expected_grid = Grid::create(5, 5, &[(2,1),(2,2),(2,3)]);
+
+    // advance the clock one generation
+    input_grid.tick();
+
+    // verify the state
+    assert_eq!(&input_grid.get_cells(), &expected_grid.get_cells());
+}
+
+// test the 3x3 blinker oscillator
+// horiz-to-vert
+#[wasm_bindgen_test]
+pub fn test_blinker_2() {
+    // Let's create a grid in known state
+    let mut input_grid = Grid::create(5, 5, &[(2,1),(2,2),(2,3)]);
+
+    // expected state after one generation
+    let expected_grid = Grid::create(5, 5, &[(1,2),(2,2),(3,2)]);
 
     // advance the clock one generation
     input_grid.tick();
